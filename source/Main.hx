0105134b4c7dcd7338b206b7cf3e5a7ee9484fc3
HyperSAM <hyperhornetsam@gmail.com>
2021-10-15 05:34:59 -0400

    new menu + mom assets

--- source/Main.hx
+++ source/Main.hx
@@ -84,42 +84,42 @@ class Main extends Sprite
 		[ [songs to use], [characters in songs], [color of week], name of week ]
 	**/
 	public static var gameWeeks:Array<Dynamic> = [
-		[['Tutorial'], ['gf'], [FlxColor.fromRGB(129, 100, 223)], 'Funky Beginnings'],
+		[['Tutorial'], ['gf'], [FlxColor.fromRGB(129, 100, 223)], 'Crystal Style'],
 		[
 			['Bopeebo', 'Fresh', 'Dadbattle'],
 			['dad', 'dad', 'dad'],
 			[FlxColor.fromRGB(129, 100, 223)],
-			'vs. DADDY DEAREST'
+			'Bars Aflame'
 		],
 		[
 			['Spookeez', 'South', 'Monster'],
 			['spooky', 'spooky', 'monster'],
 			[FlxColor.fromRGB(30, 45, 60)],
-			'Spooky Month'
+			'Candibals'
 		],
 		[
 			['Pico', 'Philly-Nice', 'Blammed'],
 			['pico'],
 			[FlxColor.fromRGB(111, 19, 60)],
-			'vs. Pico'
+			'Earthen'
 		],
 		[
 			['Satin-Panties', 'High', 'Milf'],
 			['mom'],
 			[FlxColor.fromRGB(203, 113, 170)],
-			'MOMMY MUST MURDER'
+			'Gemesis'
 		],
 		[
 			['Cocoa', 'Eggnog', 'Winter-Horrorland'],
 			['parents-christmas', 'parents-christmas', 'christmas-monster'],
 			[FlxColor.fromRGB(141, 165, 206)],
-			'RED SNOW'
+			'Freshly Snowed'
 		],
 		[
 			['Senpai', 'Roses', 'Thorns'],
 			['senpai', 'roses-senpai', 'spirit'],
 			[FlxColor.fromRGB(206, 106, 169)],
-			"hating simulator ft. moawling"
+			"The Excellent of Senpai (Real!)"
 		],
 	];
 
--- source/gameObjects/userInterface/menu/MenuCharacter.hx
+++ source/gameObjects/userInterface/menu/MenuCharacter.hx
@@ -10,16 +10,16 @@ class MenuCharacter extends FlxSprite
 	var curCharacterMap:Map<String, Array<Dynamic>> = [
 		// the format is currently
 		// name of character => id in atlas, fps, loop, scale, offsetx, offsety
-		'bf' => ["bf normal", 24, true, 1, 0, 0],
+		'bf' => ["bf normal", 24, true, 1, 1500, 0],
 		//'bfConfirm' => ["bf normal", 24, true, 1, 0, 0],
 		'xmasbf' => ['bf christmas', 24, true, 1, 0, 0],
 		'gf' => ["gf", 24, true, 1, -100, 100],
-		'dad' => ["dad", 24, true, 1, -50, 100],
+		'dad' => ["dad", 24, true, 1.25, -50, 100],
 		'spooky' => ["spooky kids", 24, true, 1, 0, 40],
 		'pico' => ["pico", 24, true, 1, -50, 150],
-		'mom' => ["mom", 24, true, 1, -50, 25],
+		'mom' => ["mom", 24, true, .75, -50, 25],
 		'parents-christmas' => ["parents", 24, true, 1.25, -100, 60],
-		'senpai' => ["senpai", 24, true, 1, -50, 0]
+		'senpai' => ["senpai", 24, true, .75, -50, 0]
 	];
 
 	var baseX:Float = 0;
@@ -40,7 +40,6 @@ class MenuCharacter extends FlxSprite
 
 	public function createCharacter(newCharacter:String, canChange:Bool = false)
 	{
-		var tex = Paths.getSparrowAtlas('menus/base/storymenu/crystal_menu_characters');
 		var tex = Paths.getSparrowAtlas('menus/base/storymenu/crystal_menu_characters');
 		frames = tex;
 		var assortedValues = curCharacterMap.get(newCharacter);
