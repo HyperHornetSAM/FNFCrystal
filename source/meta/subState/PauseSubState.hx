3965dece5a47e66f80e05440bf4e572276a170b2
Mihai Alexandru <77043862+MAJigsaw77@users.noreply.github.com>
2022-12-26 10:33:30 +0200

    Update PauseSubState.hx

--- source/meta/subState/PauseSubState.hx
+++ source/meta/subState/PauseSubState.hx
@@ -106,6 +106,11 @@ class PauseSubState extends MusicBeatSubState
 		#if debug
 		// trace('cameras done');
 		#end
+
+		#if mobile
+		addVirtualPad(UP_DOWN, A);
+		addVirtualPadCamera();
+		#end
 	}
 
 	override function update(elapsed:Float)
